ef8aedd74cb3a15a8a259851a09228f610a026e9
Dedt20 <detorresdaina1020@gmail.com>
2025-05-05 17:35:25 +0800

    Add project files.

---
+++ A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aaaAAh daddyyy!!");
+            Console.WriteLine("aaaAAh daddyyy!!");
+            Console.WriteLine("aaaAAh daddyyy!!");
+        }
+    }
+}
